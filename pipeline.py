b16943e5d94ba1d13bf2e099f210f5a3de503749
Dr. J. M. Ashfaque, MInstP, MAAT, AATQB <39216339+UKVeteran@users.noreply.github.com>
2023-10-08 10:13:38 +0100

    Add files via upload

---
+++ pipeline.py
@@ -0,0 +1,50 @@
+import logging
+from datetime import datetime
+import argparse
+
+import apache_beam as beam
+
+
+def parse_lines(element):
+    return element.split(",")
+
+
+class CalcVisitDuration(beam.DoFn):
+    def process(self, element):
+        dt_format = "%Y-%m-%dT%H:%M:%S"
+        start_dt = datetime.strptime(element[1], dt_format)
+        end_dt = datetime.strptime(element[2], dt_format)
+
+        diff = end_dt - start_dt
+
+        yield [element[0], diff.total_seconds()]
+
+
+
+
+def run(argv=None):
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--input")
+    parser.add_argument("--output")
+    args, beam_args=parser.parse_known_args(argv)
+
+    with beam.Pipeline() as p:
+        lines= (
+            p
+            | "ReadFile" >> beam.io.ReadFromText(args.input,skip_header_lines=1)
+            |  "ParseLines">>beam.Map(parse_lines)
+            )
+
+        duration =  lines | "CalcVisitDuration">> beam.ParDo(CalcVisitDuration())
+
+        duration | beam.Map(print)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.WARNING)
+    run()
